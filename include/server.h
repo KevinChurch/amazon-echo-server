e89b6259245c23fa735b0378121504a333670111
Thomas Pan <thomascpan@cs130.org>
2019-01-19 15:51:00 +0000

    Move classes into own folder: server.h, server.cc, session.h, and session.cc.
    Change-Id: I1730084f1cbe9dac64b9dd329e2f09a753f3bbc3

---
+++ include/server.h
@@ -0,0 +1,23 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <boost/asio.hpp>
+#include "session.h"
+
+using boost::asio::ip::tcp;
+
+class Server {
+public:
+  Server(boost::asio::io_service& io_service, short port);
+
+private:
+  void start_accept();
+
+  void handle_accept(Session* new_session,
+      const boost::system::error_code& error);
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+#endif
