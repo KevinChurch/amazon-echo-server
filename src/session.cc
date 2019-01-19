4cca9399e54bd9017d3fddcf66a4f0ea7d002680
Thomas Pan <thomascpan@cs130.org>
2019-01-19 15:51:00 +0000

    Move classes into own folder: server.h, server.cc, session.h, and session.cc.

---
+++ src/session.cc
@@ -0,0 +1,34 @@
+#include "session.h"
+
+Session::Session(boost::asio::io_service& io_service)
+    : socket_(io_service) { }
+
+void Session::handle_read(const boost::system::error_code& error,
+    size_t bytes_transferred) {
+    if (!error) {
+        boost::asio::async_write(socket_,
+            boost::asio::buffer(data_, bytes_transferred),
+            boost::bind(&Session::handle_write, this,
+            boost::asio::placeholders::error));
+    } else {
+        delete this;
+    }
+}
+
+void Session::handle_write(const boost::system::error_code& error) {
+    if (!error) {
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+            boost::bind(&Session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    } else {
+        delete this;
+    }
+}
+
+void Session::start() {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&Session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+}
