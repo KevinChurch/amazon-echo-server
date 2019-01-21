5976041860689d0c4cb775b59da270ed3e10888c
Thomas Pan <thomascpan@cs130.org>
2019-01-21 20:19:52 +0000

    Update integration_test.sh
    Change-Id: I8b2f56b596978e8eb4b9a7793ebe5728e6ce6222

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -4,12 +4,19 @@
 # This test can only be run on the host machine at the moment. 
 # TODO: Figure out how to run this test inside the docker container,
 # and make this part of the deployment process.
+# TODO: The strings are the same, but it looks like there might be
+# some hidden characters differences that I'm not able to detect atm.
 
-STRING1=`echo -e "hello" | nc localhost 80`
-STRING2=$'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nhello'
+STRING1=`echo -e "GET / HTTP/1.1\r\n\r\n" | nc localhost 80`
+STRING2=$'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET / HTTP/1.1\r\n'
+
+# echo "$STRING1"
+# echo "$STRING2"
 
 if [ "$STRING1" == "$STRING2" ]; then
     echo "pass"
 else
     echo "fail"
 fi
+
+# diff <(echo "$STRING1") <(echo "$STRING2")
