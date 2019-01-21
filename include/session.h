7dc007995d8b9a1d2460634de7fdbcc099a77b40
Kon Son Lam <konsonlam@cs130.org>
2019-01-21 06:59:46 +0000

    update session.h, session.cc & CMakeLists.txt
    Change-Id: Ib9f4820201ec9e4041d1f9371141daefdc73c40e

--- include/session.h
+++ include/session.h
@@ -18,11 +18,18 @@ public:
 
   void start();
 
+  int handle_request();
+
+  void write_string(std::string send);
+
 private:
     void handle_read(const boost::system::error_code& error,
         size_t bytes_transferred);
 
     void handle_write(const boost::system::error_code& error);
+
+    std::string get_message_request();
+    boost::asio::streambuf buffer;
 
   tcp::socket socket_;
   enum { max_length = 1024 };
--- src/session.cc
+++ src/session.cc
@@ -1,4 +1,13 @@
 #include "session.h"
+#include "request.h"
+//#include "response.h"
+
+#include <termios.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include <memory>
 
 Session::Session(boost::asio::io_service& io_service)
     : socket_(io_service) { }
@@ -31,4 +40,45 @@ void Session::start() {
         boost::bind(&Session::handle_read, this,
             boost::asio::placeholders::error,
             boost::asio::placeholders::bytes_transferred));
+}
+
+int Session::handle_request(){
+
+  auto request = Request::ParseRequest(get_message_request());
+// TODO create response
+
+  //response.SetStatus(200);
+  //response.SetHeader("Content-Type", "text/plain");
+  //response.SetBody(request->original_request());
+  //write_string(response.ToString());
+  return 0;
+}
+
+
+
+void Session::write_string(std::string send) {
+
+  boost::asio::streambuf out_streambuf;
+  std::ostream out(&out_streambuf);
+  out << send;
+
+  boost::asio::write(socket_, out_streambuf);
+
+ 
+  boost::system::error_code ec;
+  socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
+  socket_.close();
+}
+
+
+
+
+std::string Session::get_message_request()
+{
+  std::string msg{
+    buffers_begin(buffer.data()),
+    buffers_end(buffer.data())
+  };
+
+  return msg;
 }
