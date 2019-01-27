f80f9ebb79e6ed6ea689c49d8a4eca08a4f9083d
Kon Son Lam <konsonlam@cs130.org>
2019-01-27 01:52:15 +0000

    Add more unit tests for request_test.cc and correct the typo for CheckUri
    Change-Id: I09a6a0ba64ffa90e0dbe3486ae1aa6ed2ee89738

--- tests/request_test.cc
+++ tests/request_test.cc
@@ -10,29 +10,44 @@ using namespace std;
 class RequestTest:public::testing::Test{
 	protected:
 		string s = "GET / HTTP/1.1\r\n\r\n";
+		string r = "GO | HTTP/1.0\r\n\r\n";
+		unique_ptr<Request> request = Request::ParseRequest(s);
+		unique_ptr<Request> invalidrequest = Request::ParseRequest(r);
 		bool succes;
 
 };
 
 
 TEST_F(RequestTest, CheckOriginal_request){
-	auto request = Request::ParseRequest(s);
 	EXPECT_EQ(request->original_request(),"GET / HTTP/1.1\r\n\r\n");
 }
 
+TEST_F(RequestTest, CheckInvalidOriginal_request){
+	EXPECT_NE(invalidrequest->original_request(),"GET / HTTP/1.1\r\n\r\n");
+}
+
 TEST_F(RequestTest, CheckMethod){
-	auto request = Request::ParseRequest(s);
 	EXPECT_EQ(request->method(),"GET");
 }
 
-TEST_F(RequestTest, ChechkUri){
-	auto request = Request::ParseRequest(s);
+TEST_F(RequestTest, CheckInvaildMethod){
+	EXPECT_NE(invalidrequest->method(),"GET");
+}
+
+TEST_F(RequestTest, CheckUri){
 	EXPECT_EQ(request->uri(),"/");
 }
 
+TEST_F(RequestTest, CheckInvalidUri){
+	EXPECT_NE(invalidrequest->uri(),"/");
+}
+
 TEST_F(RequestTest, CheckVersion){
-	auto request = Request::ParseRequest(s);
 	EXPECT_EQ(request->version(),"HTTP/1.1");
+}
+
+TEST_F(RequestTest, CheckInvalidVersion){
+	EXPECT_NE(invalidrequest->version(),"HTTP/1.1");
 }
 
 
