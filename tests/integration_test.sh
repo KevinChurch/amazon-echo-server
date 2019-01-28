0368f946e7805a6a3b23d9027b351f2fc966c653
Thomas Pan <thomascpan@cs130.org>
2019-01-28 06:13:13 +0000

    Add netcat to base.Dockerfile. Add dev_config. Add integration test to CMakeList.txt. Update integration_test.sh.
    Change-Id: I9843bf0bab3f13fa426cd5c92153d27aa073c5ef

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -4,8 +4,29 @@
 # This test can only be run on the host machine at the moment. 
 # TODO: Figure out how to run this test inside the docker container,
 # and make this part of the deployment process.
-# TODO: The strings are the same, but it looks like there might be
-# some hidden characters differences that I'm not able to detect atm.
+
+CWD="$(pwd)" 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+goto() {
+    cd "$1"
+}
+
+run_server() {
+    goto "$DIR"
+    echo "../build/bin/server ../dev_config &"
+    ../build/bin/server ../dev_config &
+    SERVER_PID=$!
+    # TODO: Figure out how to make the rest of the script wait 
+    # for the server to stat up instead of usign sleep. 
+    sleep 5
+    goto "$CWD"
+}
+
+kill_server() {
+    echo "kill -9 $SERVER_PID"
+    kill -9 "$SERVER_PID"
+}
 
 test() {
     if [ "$1" == "$2" ]; then
@@ -14,11 +35,15 @@ test() {
         echo "fail"
         echo "$1" | od -c
         echo "$2" | od -c
+        kill_server
+        exit 1
     fi
 }
 
+run_server
+
 # GOOD REQUEST
-STRING_INPUT_1=`echo -e "GET / HTTP/1.1\r\n\r\n" | nc localhost 8080`
+STRING_INPUT_1=`echo -e "GET / HTTP/1.1\r\n\r\n" | netcat localhost 8080`
 STRING_OUTPUT_1=$'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET / HTTP/1.1\r\n\r\n'
 STRING_OUTPUT_1=${STRING_OUTPUT_1%$'\n'} # Removes automatic newline added to end of variable.
 test "$STRING_INPUT_1" "$STRING_OUTPUT_1"
@@ -29,12 +54,16 @@ STRING_OUTPUT_2=${STRING_OUTPUT_2%$'\n'} # Removes automatic newline added to en
 test "$STRING_INPUT_2" "$STRING_OUTPUT_2"
 
 # BAD REQUESTS
-# STRING_INPUT_3=`echo -e "GET / HTTP\r\n\r\n" | nc localhost 8080`
+# STRING_INPUT_3=`echo -e "GET / HTTP\r\n\r\n" | netcat localhost 8080`
 # STRING_OUTPUT_3=$'HTTP/1.0 400 Bad Request\r\nContent-Type: text/plain\r\n\r\n'
 # STRING_OUTPUT_3=${STRING_OUTPUT_3%$'\n'} # Removes automatic newline added to end of variable.
 # test "$STRING_INPUT_3" "$STRING_OUTPUT_3"
 
-# STRING_INPUT_4=`echo -e "GEET / HTTP/1.1\r\n\r\n" | nc localhost 8080`
+# STRING_INPUT_4=`echo -e "GEET / HTTP/1.1\r\n\r\n" | netcat localhost 8080`
 # STRING_OUTPUT_4=$'HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\n'
 # STRING_OUTPUT_4=${STRING_OUTPUT_4%$'\n'} # Removes automatic newline added to end of variable.
 # test "$STRING_INPUT_4" "$STRING_OUTPUT_4"
+
+kill_server
+
+exit 0
