ffe5642c386d75d000e2f9e870fc5112ee68837c
Thomas Pan <thomascpan@cs130.org>
2019-01-29 04:45:51 +0000

    Add session_test.cc and update CMakeList.txt
    Change-Id: I20b22a949e53698071f30c893c255c6f230d4229

---
+++ tests/session_test.cc
@@ -0,0 +1,42 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "session.h"
+
+// class MockSession : public Session {
+// public:
+//     MockSession(boost::asio::io_service& io_service) : Session(io_service) {}
+// };
+
+// class MockIOService : public boost::asio::io_service {
+// public:
+//     MOCK_METHOD1(Equals, bool(const MockIOService &));
+//     virtual bool operator==(const MockIOService & rhs) { return Equals(rhs); }
+// };
+
+// class SessionTest : public ::testing::Test {
+// protected:
+//     MockIOService io_service_;
+//     Session* new_session = new Session(io_service_);
+// };
+
+// TEST_F(SessionTest, Socket) {
+//     EXPECT_EQ(new_session->socket(), io_service_);
+// }
+
+// TEST_F(SessionTest, Start) {
+//     std::string response = sess->get_response();
+//     EXPECT_FALSE(response.empty());
+//     EXPECT_EQ(response.substr(0, 17), "HTTP/1.1 200 OK\r\n");
+// }
+
+// TEST_F(SessionTest, HandleRequest) {
+//     std::string response = sess->get_response();
+//     EXPECT_FALSE(response.empty());
+//     EXPECT_EQ(response.substr(0, 17), "HTTP/1.1 200 OK\r\n");
+// }
+
+// TEST_F(SessionTest, WriteString) {
+//     std::string response = sess->get_response();
+//     EXPECT_FALSE(response.empty());
+//     EXPECT_EQ(response.substr(0, 17), "HTTP/1.1 200 OK\r\n");
+// }
