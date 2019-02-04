7047a44ef52096fc53d0f0b47158b060809e4fbb
Kon Son Lam <konsonlam@cs130.org>
2019-02-04 03:46:52 +0000

    Update handler.h and handler.cc
    Change-Id: I21640dcf3b1002a78f4f1698ad4cf83beb1c31b8

--- include/handler.h
+++ include/handler.h
@@ -4,6 +4,7 @@
 #include "response.h"
 #include "request.h"
 #include "config_parser.h"
+#include <map>
 
 class Handler{
  public:
@@ -13,18 +14,57 @@ class Handler{
 }
 
 
-
-class EchoHandler: public Handler {
+extern std::map<std::string, Handler* (*)(void)>* request_handler_builders;
+template<typename T>
+class RequestHandlerRegisterer {
  public:
-  EchoHandler() {}
-}
+  RequestHandlerRegisterer(const std::string& type) {
+    if (request_handler_builders == nullptr) {
+      request_handler_builders = new std::map<std::string, Handler* (*)(void)>;
+    }
+    (*request_handler_builders)[type] = RequestHandlerRegisterer::Create;
+  }
+  static Handler* Create() {
+    return new T;
+  }
+};
+#define REGISTER_REQUEST_HANDLER(ClassName) \
+  static RequestHandlerRegisterer<ClassName> ClassName##__registerer(#ClassName)
+
+
+
+// simple handler to echo back raw response
+class EchoHandler : public Handler {
+public:
+    EchoHandler() {}
+
+    bool Init(const std::string& uri_prefix, const NginxConfig& config);
+
+    bool HandleRequest(const Request& request, Response* response);
+private:
+    std::string to_send;
+};
+
+REGISTER_REQUEST_HANDLER(EchoHandler);
+
+
 
 class StaticHandler: public Handler {
  public:
   StaticHandler() {}
 }
 
-class NotFoundHandler: public Handler{ 
-  public:
-   NotFoundHandler() {}
-}
+
+REGISTER_REQUEST_HANDLER(StaticHandler);
+
+class NotFoundHandler : public Handler {
+public:
+    NotFoundHandler() {}
+
+    bool Init(const std::string& uri_prefix, const NginxConfig& config);
+
+    bool HandleRequest(const Request& request, Response* response);
+};
+
+
+REGISTER_REQUEST_HANDLER(NotFoundHandler);
