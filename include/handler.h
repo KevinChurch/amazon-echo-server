312616ff965fdb8a7ed519cf355e1b6b25a00aae
Thomas Pan <thomascpan@cs130.org>
2019-02-04 22:10:23 +0000

    Revert "Update handler.h and handler.cc"
    This reverts commit 7047a44ef52096fc53d0f0b47158b060809e4fbb.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: Icb08322746c5fb54f1ecb2298d12f8dbfcd86125

--- include/handler.h
+++ include/handler.h
@@ -4,7 +4,6 @@
 #include "response.h"
 #include "request.h"
 #include "config_parser.h"
-#include <map>
 
 class Handler{
  public:
@@ -14,57 +13,18 @@ class Handler{
 }
 
 
-extern std::map<std::string, Handler* (*)(void)>* request_handler_builders;
-template<typename T>
-class RequestHandlerRegisterer {
+
+class EchoHandler: public Handler {
  public:
-  RequestHandlerRegisterer(const std::string& type) {
-    if (request_handler_builders == nullptr) {
-      request_handler_builders = new std::map<std::string, Handler* (*)(void)>;
-    }
-    (*request_handler_builders)[type] = RequestHandlerRegisterer::Create;
-  }
-  static Handler* Create() {
-    return new T;
-  }
-};
-#define REGISTER_REQUEST_HANDLER(ClassName) \
-  static RequestHandlerRegisterer<ClassName> ClassName##__registerer(#ClassName)
-
-
-
-// simple handler to echo back raw response
-class EchoHandler : public Handler {
-public:
-    EchoHandler() {}
-
-    bool Init(const std::string& uri_prefix, const NginxConfig& config);
-
-    bool HandleRequest(const Request& request, Response* response);
-private:
-    std::string to_send;
-};
-
-REGISTER_REQUEST_HANDLER(EchoHandler);
-
-
+  EchoHandler() {}
+}
 
 class StaticHandler: public Handler {
  public:
   StaticHandler() {}
 }
 
-
-REGISTER_REQUEST_HANDLER(StaticHandler);
-
-class NotFoundHandler : public Handler {
-public:
-    NotFoundHandler() {}
-
-    bool Init(const std::string& uri_prefix, const NginxConfig& config);
-
-    bool HandleRequest(const Request& request, Response* response);
-};
-
-
-REGISTER_REQUEST_HANDLER(NotFoundHandler);
+class NotFoundHandler: public Handler{ 
+  public:
+   NotFoundHandler() {}
+}
--- src/handler.cc
+++ src/handler.cc
@@ -1,39 +1,19 @@
 #include "handler.h"
 #include "response.h"
 #include "request.h"
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 
-std::map<std::string, Handler* (*)(void)>* request_handler_builders = nullptr;
+Handler* Handler::GetHandlerByType(char* type){
 
-Handler* Handler::GetHandlerByType(const char* type) {
-  const auto type_and_builder = request_handler_builders->find(type);
-  if (type_and_builder == request_handler_builders->end()) {
-    return nullptr;
-  }
-  return (*type_and_builder->second)();
+  return nullptr;
 }
 
-
-
-Handler::bool EchoHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
-    return true;
+bool EchoHandler::Init(const std::string& uri_prefix, const NginxConfig& config){
+  return false;
 }
 
-
-//Echo handler
-Handler::bool EchoHandler::HandleRequest(const Request& request, Response* response) {
-    std::cout << "\nEchoHandler::HandleRequest" << std::endl;
-
-    response->SetStatus(200);
-    response->AddHeader("Content-Type", "text/plain");
-    response->SetBody(request.original_request());
-    return true;
+bool EchoHandler::HandleRequest(const Request& request, const NginxConfig& config){
+  return false;
 }
-
-
 
 bool StaticHandler::Init(const std::string& uri_prefix, const NginxConfig& config){
   return false;
@@ -43,19 +23,10 @@ bool StaticHandler::HandleRequest(const Request& request, const NginxConfig& con
   return false;
 }
 
-
-
-Handler::bool NotFoundHandler::Init(const std::string& uri_prefix, const NginxConfig& config) {
-    return true;
+bool NotFoundHandler::Init(const std::string& uri_prefix, const NginxConfig& config){
+  return false;
 }
 
-//Not Found handler 
-Handler::bool NotFoundHandler::HandleRequest(const Request& request, Response* response){
-    std::cout << "\nNotFoundHandler::HandleRequest" << std::endl;
-
-    std::string body = "404 NOT FOUND";
-    response->SetStatus(404);
-    response->AddHeader("Content-Type", "text/plain");
-    response->SetBody(body);
-    return true;
+bool NotFoundHandler::HandleRequest(const Request& request, const NginxConfig& config){
+  return false;
 }
